b67edb7be85441d4e6510a5d308d26b1d68917f5
radheem <radheem@seedcode.io>
2021-01-15 22:39:52 +0500

    dbjajkldbjkas

--- src/macrosutils.rs
+++ src/macrosutils.rs
@@ -1,9 +1,20 @@
-#[macro_export]
+#[allow(dead_code)]
+use std::collections::HashMap;
 
+#[macro_export]
 macro_rules! say_hello {
-    // `()` indicates that the macro takes no argument.
     () => {
         // The macro will expand into the contents of this block.
         println!("Hello!");
     };
+}
+#[macro_export]
+macro_rules! mapping{
+    ($($key:expr => $val:expr),+) => {
+        let mut map = HashMap::new();
+        $(
+            map.insert($key,$val);
+        )+
+    map
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,49 @@
 #[macro_use]
 // imports
-extern crate macrosutils;
+// mod macrosutils;
 mod utils;
-use macrosutils::say_hello;
 
 
 
 //declarations 
 #[derive(Debug)]
-struct Accountid{
-    epoch:i32,
-    hash: &'static str
+pub struct UserAccount<T,G>{
+    pk: T,
+    epoch:G,
+    hash: &'static str,
+    amount: i32
 }
-impl Accountid{
-    pub fn new(epoch:i32,hash: &'static str)-> Accountid{
-        Accountid{
+trait Account{
+    fn new<T,G>(pk: T,epoch:G,hash: &'static str)-> Self;
+    fn transfer<T>(x:&mut T,y:&mut T,amount: i32);
+    fn credit<T>(acc: &mut T,amount:i32);
+    fn debit<T>(acc: &mut T,amount:i32);
+}
+impl Account for UserAccount{
+    fn new<T,G>(pk: T, epoch:G, hash: &'static str)-> UserAccount{
+        UserAccount{
+            pk: pk,
             epoch:epoch,
             hash: hash,
+            amount: 0
         }
+    }
+    fn transfer<UserAccount>(x:&mut UserAccount,y: &mut UserAccount,amount: i32){
+        assert!(x.amount > amount,"the sender doesn't have enough funds");
+        x.amount -= amount;
+        y.amount += amount;
+    }
+    fn credit<UserAccount>(acc: &mut UserAccount,amount:i32){
+        acc.amount += amount;
+    }
+    fn debit<UserAccount>(acc: &mut UserAccount,amount:i32){
+        assert!(acc.amount >= amount,"the sender doesn't have enough funds");
+        acc.amount -= amount;
     }
 }
 
 
 fn main() {
-    let _str = String::from(9.to_string());
-    let user1 = Accountid::new(5,"f10xh123546821354353213");
-    utils::print(&_str);
+    let user1 = UserAccount::new::<i64,i32>(146186156816261536,5,"f10xh123546821354353213");
     utils::print(&user1);
-    self::say_hello!{};
 }
