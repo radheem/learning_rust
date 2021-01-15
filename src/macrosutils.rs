ae8f29eec318faa2e0a22cd50c2b8a5226b6f881
radheem <radheem@seedcode.io>
2021-01-15 17:49:34 +0500

    added macrosutils.rs

---
+++ src/macrosutils.rs
@@ -0,0 +1,9 @@
+#[macro_export]
+
+macro_rules! say_hello {
+    // `()` indicates that the macro takes no argument.
+    () => {
+        // The macro will expand into the contents of this block.
+        println!("Hello!");
+    };
+}
